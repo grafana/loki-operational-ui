2e0427a7d7bb758bb6a3b4c7aa22c7d3262a3168
Dan Hopper <dan.hopper@grafana.com>
2026-05-29 16:28:41 +0100

    chore: remove tailwindcss-animate dependency
    This was unused, only referenced by unused config in `tailwind.config.js`.

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,28 +11,6 @@ export default {
       },
     },
     extend: {
-      keyframes: {
-        'accordion-down': {
-          from: {
-            height: 0,
-          },
-          to: {
-            height: 'var(--radix-accordion-content-height)',
-          },
-        },
-        'accordion-up': {
-          from: {
-            height: 'var(--radix-accordion-content-height)',
-          },
-          to: {
-            height: 0,
-          },
-        },
-      },
-      animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
-      },
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
@@ -92,5 +70,5 @@ export default {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [],
 };
